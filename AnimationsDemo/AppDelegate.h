8a4f4b5f2f7b04c5ada3ff50d13ed91927bbc631
gleeeli <gleeeli@sina.com>
2017-12-07 13:59:46 +0800

    Initial Commit

---
+++ AnimationsDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimationsDemo
+//
+//  Created by zqh on 2017/12/7.
+//  Copyright © 2017年 WeiEr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnimationsDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnimationsDemo
+//
+//  Created by zqh on 2017/12/7.
+//  Copyright © 2017年 WeiEr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
